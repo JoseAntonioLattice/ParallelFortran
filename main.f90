0e0ec8c10981cbe7c3f945a30c4f70449788764d
pepe <antonio.garcia.lattice@gmail.com>
2025-03-09 20:47:11 -0600

    divide lattice into rectangles

--- main.f90
+++ main.f90
@@ -4,24 +4,24 @@ program main
 
   real :: x(3), y(3)
   integer :: d(2), col ,row
-  integer, parameter :: L = 16
+  integer, dimension(2), parameter :: L = [20,16]
   integer :: k, p(2,2)
   integer :: i, j, tile_sizex, tile_sizey
   integer, allocatable :: s(:,:)[:], sglobal(:,:)[:] , indices(:,:)
+  integer :: i1(2), j1(2)
 
   
   !call random_init(.false.,.true.)
   !call random_number(x)
   !print*, this_image(), x
+  d = [5,1]
+  tile_sizex = L(1)/d(1)
+  tile_sizey = L(2)/d(2)
 
-
+  print*, any(L/d /= 0)
   
-  d = [2,2]
-  tile_sizex = L/d(1)
-  tile_sizey = L/d(2)
-
   allocate(s(1:tile_sizex,1:tile_sizey)[*])
-  allocate(sglobal(L,L)[*])
+  allocate(sglobal(L(1),L(2))[*])
   allocate(indices(2,2))
 
   k = 0
@@ -31,44 +31,44 @@ program main
      end do
   end do
 
-  if( mod(this_image(),d(1)) == 0)then
-     col = this_image()/d(1)
+  if(mod(this_image(),d(1)) == 0) then
+     row = d(1)
   else
-     col = this_image()/d(1) + 1
+     row = mod(this_image(),d(1))
   end if
 
-  
-  if( mod(this_image(),d(2)) == 0)then
-     row = d(2)
+  if(mod(this_image(),d(1)*d(2)) /= 0)then
+     col = (mod(this_image(),d(1)*d(2)) - row)/d(1)+1
   else
-     row = mod(this_image(),d(2))
+     col = (d(1)*d(2) - row)/d(1)+1
   end if
+  sync all
+  print'("image = ",i1,x, "(", i1, ",", i1, ") ", 2(i2,x, i2))', This_image(), row, col, tile_indices(L(1),row,d(1))&
+       , tile_indices(L(2),col,d(2))
+  i1 = tile_indices(L(1),row,d(1))
+  j1 = tile_indices(L(2),col,d(2))
 
-  !print*, This_image(), col, row
-  indices(1,:) = tile_indices(L,col,d(1))
-  indices(2,:) = tile_indices(L,row,d(2))
-
-
-  !print*, indices(1,:), indices(2,:)
+  s = this_image()
   
-  s(:,:) = this_image()
   
-  sglobal(indices(1,1):indices(1,2),indices(2,1):indices(2,2))[1] = s(:,:)
+  sglobal(i1(1):i1(2),j1(1):j1(2))[1] = s
   sync all
   if (this_image() == 1) then
-     do i = 1, L
-        print*, sglobal(i,:)
+     do i = 1, L(1)
+        print'(*(i1,x))', sglobal(i,:)
      end do
   end if
-  
+
 contains
 
-  pure function tile_indices(L,im,n)    
+  function tile_indices(L,im,n)
+    implicit none
     integer, intent(in)  :: L, im, n
     integer, dimension(2):: tile_indices
 
     tile_indices(1) = (L * (im - 1) ) / n + 1 
     tile_indices(2) = (L * im ) / n
+    
   end function tile_indices
 
 end program main
